4b37caf58a717b7c01f4340a7d4e8518ea96179b
Jordan <jordan.jayol@etu.univ-lyon1.fr>
2018-03-29 12:08:22 +0200

    MàJ BDD

--- rc1-linky/database/migrations/2014_10_12_000000_create_users_table.php
+++ rc1-linky/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,11 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('logement');
+            $table->string('superficie');
+            $table->string('habitants');
             $table->rememberToken();
             $table->timestamps();
         });
