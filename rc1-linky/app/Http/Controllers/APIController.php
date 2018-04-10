7dbc2c589456ce3bea4fdf0577f12068e9da0278
Alexandre Bruyat <contact@alexandre-bruyat.fr>
2018-04-10 15:52:09 +0200

    debut Queue recup API

--- rc1-linky/app/Http/Controllers/APIController.php
+++ rc1-linky/app/Http/Controllers/APIController.php
@@ -4,15 +4,15 @@
 namespace App\Http\Controllers;
 
 
-use App\Parser\Parser;
-use DateInterval;
-use DateTime;
-use GuzzleHttp\Client;
+use App\Jobs\RetrieveEnedisData;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Support\Facades\Auth;
+
 
 class APIController extends Controller
 {
 
+    use DispatchesJobs;
     /**
      * @throws \GuzzleHttp\Exception\GuzzleException
      * @throws \Exception
@@ -24,36 +24,8 @@ class APIController extends Controller
             $status['status']='ko';
             $status['error']="Vous devez renseigner vos informations de connexion enedis";
         }else {
-            $begin = new DateTime();
-            $begin->sub(new DateInterval('P1Y'));
-            $client = new Client();
-            $res = $client->request('POST', 'http://api/', [
-                'form_params' => [
-                    'login' => $user->login_linky,
-                    'mdp' => $user->mdp_linky,
-                    'type' => 'hour',
-                    'debut' => $begin->format('d/m/Y'),
-                    'fin' => date('d/m/Y')
-                ]
-            ]);
-
-            $result = json_decode($res->getBody(), true);
-            if ($result['status'] == "ok") {
-                $parser = new Parser();
-                $last=0;
-                foreach ($result['data'] as $conso) {
-                    if($last==$conso['conso'] && $last==0){
-                        $last=$conso['conso'];
-                    }else {
-                        $parser->save(DateTime::createFromFormat("d-m-Y-H:i", str_replace_first('+', '-', str_replace_first('h', ':', $conso['time']))), floatval($conso['conso']), $user->id);
-                    }
-                }
-            }else{
-                $status['status']="ko";
-                $status['error']=$result['error'][0];
-            }
+            $this->dispatch(new RetrieveEnedisData($user));
         }
-
         echo response()->json($status);
     }
 
