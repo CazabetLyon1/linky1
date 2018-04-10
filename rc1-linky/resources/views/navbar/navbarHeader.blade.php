68bf74ede3d6b3d9ab83cef8b147b3328eed62e2
p1706966 <adrien.mevelec@hotmail.fr>
2018-04-10 14:44:39 +0200

    removed alerts and the fck >

--- rc1-linky/resources/views/navbar/navbarHeader.blade.php
+++ rc1-linky/resources/views/navbar/navbarHeader.blade.php
@@ -15,19 +15,6 @@
 
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
-                <!-- Tasks: style can be found in dropdown.less -->
-                <li class="dropdown tasks-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="fa fa-flag-o"></i>
-                        <span class="label label-danger">9</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="header">Vous avez 9 alertes</li>
-                        <li class="footer">
-                            <a href="#">Voir les alertes</a>
-                        </li>
-                    </ul>
-                </li>
                 <!-- User Account: style can be found in dropdown.less -->
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
