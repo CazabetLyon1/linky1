f1856f05a8eb7ca90b3ee82cb24489926f2813cc
Alexandre BRUYAT <contact@alexandre-bruyat.fr>
2018-04-11 11:32:36 +0200

    MàJ controller fonctionnel

--- rc1-linky/app/Http/Controllers/ValidationController.php
+++ rc1-linky/app/Http/Controllers/ValidationController.php
@@ -5,31 +5,26 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ValidationRequest;
 use Illuminate\Http\Requests;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
 
 class ValidationController extends Controller
 {
-    public function ValidationForm()
+    /*public function ValidationForm()
     {
         return view('pages.parametersEdit');
-    }
+    }*/
 
-    public function userForm(ValidationRequest $request)
+    public function userForm()
     {
-        $input = $request->only('prenom','name','login','pswd','ville','logement','superficie','habitants');
-
-        $user = Auth::user();
 
 
-        $user->prenom = $request['prenom'];
-        $user->name = $input['name'];
-        $user->login = $input['login'];
-        $user->pswd = $input['pswd'];
-        $user->ville = $input['ville'];
-        $user->logement = $input['logement'];
-        $user->superficie = $input['superficie'];
-        $user->habitants = $input['habitants'];
+        DB::table('users')
+            ->where('id', Auth::id())
+            ->update(
+                ['prenom' => 1, 'name' => 1, 'login_linky' => 1, 'mdp_linky' => 1, 'ville' => 1, 'logement' => 1, 'superficie' => 1, 'habitants' => 1]
 
-        $user->save();
+            );
 
         return 'Mise à jour du profil';
     }
