8618fef4e5dfd7538e49cecded82ded1b89c9ad9
p1706966 <adrien.mevelec@hotmail.fr>
2018-04-19 11:50:19 +0200

    import chart js correct

--- rc1-linky/resources/views/layouts/master.blade.php
+++ rc1-linky/resources/views/layouts/master.blade.php
@@ -7,24 +7,25 @@
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <!-- Bootstrap 3.3.7 -->
-        <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href=" bower_components/bootstrap/dist/css/bootstrap.min.css  ">
+
         <!-- Font Awesome -->
-        <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href=" bower_components/font-awesome/css/font-awesome.css  ">
         <!-- Ionicons -->
-        <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+        <link rel="stylesheet" href=" bower_components/Ionicons/css/ionicons.min.css   ">
         <!-- Theme style -->
-        <link rel="stylesheet" href="css/AdminLTE.min.css">
+        <link rel="stylesheet" href=" css/AdminLTE.min.css   ">
         <!-- AdminLTE Skins. Choose a skin from the css/skins
              folder instead of downloading all of them to reduce the load. -->
-        <link rel="stylesheet" href="css/skins/_all-skins.min.css">
+        <link rel="stylesheet" href=" css/skins/_all-skins.min.css '  ">
         <!-- jvectormap -->
-        <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
+        <link rel="stylesheet" href=" bower_components/jvectormap/jquery-jvectormap.css   ">
         <!-- Date Picker -->
-        <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+        <link rel="stylesheet" href=" bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css   ">
         <!-- Daterange picker -->
-        <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
+        <link rel="stylesheet" href=" bower_components/bootstrap-daterangepicker/daterangepicker.css   ">
         <!-- bootstrap wysihtml5 - text editor -->
-        <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+        <link rel="stylesheet" href="  plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css   ">
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -46,8 +47,6 @@
         <script>
             $.widget.bridge('uibutton', $.ui.button);
         </script>
-        <!-- ChartJS -->
-        <script src="bower_components/chart.js/Chart.js"></script>
         <!-- Bootstrap 3.3.7 -->
         <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
         <!-- Sparkline -->
